e2d9b8132fadaeb3e7000928ad6ec02d6c96ae68
RichtersHotline <KeenanTimmieWeb@gmail.com>
2024-06-25 15:33:45 +0200

    navbar styling

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,32 @@
 <template>
   <nav>
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/about">About</router-link> |
+    <router-link to="/about">Resume</router-link> |
+    <router-link to="/about">Skills</router-link> |
+    <router-link to="/about">Education</router-link> |
+    <router-link to="/about">Work Experience</router-link> |
+    <router-link to="/about">Projects</router-link> |
+    <router-link to="/about">Testimonials</router-link> |
+    <router-link to="/about">Reach Me</router-link> 
+
+
+
+
+
+
   </nav>
   <router-view/>
 </template>
 
 <style>
+
+template {
+
+
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  
   text-align: center;
   color: #2c3e50;
   background-color:#332234;
@@ -18,12 +34,23 @@
 }
 
 nav {
-  padding: 30px;
+  padding: 25px;
+  font-family: "Inter", sans-serif;
+
+
+  
+}
+nav a:hover {
+  color:#9d5ea2;
+  text-decoration: underline;
+  
+
 }
 
 nav a {
   font-weight: bold;
   color: white;
+  text-decoration:none;
 }
 
 nav a.router-link-exact-active {
